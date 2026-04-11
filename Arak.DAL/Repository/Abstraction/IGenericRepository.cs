c0db05e7cb19ed06d853723c3faf27cdce667abd
ibrahim05said <is8035970@gmail.com>
2026-04-11 14:48:11 +0200

    addition forgotten ;

--- Arak.DAL/Repository/Abstraction/IGenericRepository.cs
+++ Arak.DAL/Repository/Abstraction/IGenericRepository.cs
@@ -9,7 +9,7 @@ namespace Arak.DAL.Repository.Abstraction
 	public interface IGenericRepository<T> where T : class
 	{
 		Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(int id)
+		Task<T> GetByIdAsync(int id);
         Task<T> CreateAsync(T entity);
 		Task<T> UpdateAsync(T entity);
 		Task<bool> DeleteAsync(int id);
